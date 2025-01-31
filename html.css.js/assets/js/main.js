911808cc9b8f210a93e8c0202595aa88592ad327
Nicolas Alves <nicolasalvessilva2006@gmail.com>
2025-01-30 22:14:58 -0300

    Atualização

--- html.css.js/assets/js/main.js
+++ html.css.js/assets/js/main.js
@@ -1,43 +1,39 @@
+const limit = 10;
+let offset = 0;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="numero">#001</span>
+            <span class="nome">${pokemon.name}</span>
 
-function convetpokemontoli(pokemon){
-    return`
-
-
-            
-            <li class="pokemon">
-                <span class="numero">#001</span>
-                <samp class="nome"> ${pokemon.name}</samp>
-
-                <div class="detali">
-
-                    <ol class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
-                    </ol>
-                    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1f619ed0-b566-4538-8392-bf02ca7a76cd/dck5gop-42198a25-24ed-47b3-b061-17b61d933fdd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFmNjE5ZWQwLWI1NjYtNDUzOC04MzkyLWJmMDJjYTdhNzZjZFwvZGNrNWdvcC00MjE5OGEyNS0yNGVkLTQ3YjMtYjA2MS0xN2I2MWQ5MzNmZGQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SJag-8wJaCxlLc25L9bORY6Un2PKFOHS04hbbMh5O6U"
-                        alt="${pokemon.name}">
-                    </div>
-            </li>
-      
-
-    `
+            <div class="detali">
+                <ol class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+                <img src="https://i0.wp.com/assets.b9.com.br/wp-content/uploads/2014/08/bulbasaur.png?fit=958%2C834&ssl=1"
+                alt="${pokemon.name}">
+            </div>
+        </li>
+    `;
 }
-const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getpokens().then((pokemons =[]) => { // Aqui recebo os dados da API (a lista de Pokémon) e os armazeno na variável 'pokemonList'
-        
-    const novalista =pokemons.map((pokemon)=> {
-        return convetpokemontoli(pokemon)
-    }) 
-    const novoHtml =novalista.join('')
-    pokemonList.innerHTML +=novoHtml
-        
-    //for (let i = 0; i < pokemons.length; i++) {//Geito mais dificil de fazer 
-    //        const pokemon = pokemons[i];
-    //        pokemonList.innerHTML +=convetpokemontoli(pokemon)
-     //   }
-    })
-    .catch((error) => console.error(error)) // Caso algo dê errado, exibe o erro no console
 
-    
+const pokemonList = document.getElementById('pokemonList');
+pokeApi.getPokemons().then((pokemons = []) => { // Corrigido o nome da função
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join(''); // Uso correto de innerHTML
+});
+
+
+
+//for (let i = 0; i < pokemons.length; i++) {//Geito mais dificil de fazer 
+//        const pokemon = pokemons[i];
+//        pokemonList.innerHTML +=convetpokemontoli(pokemon)
+//   }
+//    })
+//.catch((error) => console.error(error)) // Caso algo dê errado, exibe o erro no console
+
+
+
--- html.css.js/assets/js/poke-api.js
+++ html.css.js/assets/js/poke-api.js
@@ -1,10 +1,19 @@
 const pokeApi ={}
-pokeApi.getpokens =(offset =0,limit =10)=>{
+pokeApi.getPokemons =(offset =0,limit =10)=>{
     //const offset = 0;//aqui estou definido a variavel como 0
     //const limit = 10;//aqui estou definido a variavel como 10
     //defini as variavel que futuramente posso fazer mais pagina puxando as informacoes da api com essa variavel.
     const url = `https://pokeapi.co/api/v2/pokemon?offset${offset}&limit=${limit}`
+
     return fetch(url) // Aqui estou fazendo a requisição para a API usando a URL fornecida
     .then((response) => response.json()) // Aqui verifico se a requisição foi bem-sucedida e converto a resposta para JSON
     .then((jsonBody)=>jsonBody.results)
+    .then((pokemons)=>pokemons.map((pokemons) =>fetch(pokemons.url).json()))
+    .then((detailRequests) =>Promise.all(detailRequests))//lista de promeca sendo resolvida
 }
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+])
