09fade463c1858d2ee0c762466ac8720a47f30ed
Nicolas Alves <nicolasalvessilva2006@gmail.com>
2025-02-17 10:54:11 -0300

    Atualização

--- html.css.js/assets/js/main.js
+++ html.css.js/assets/js/main.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const limit = 10;
-let offset = 0;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}"><!-- Aqui seria eu pegando o tipo do pokemon--!>
-            <span class="numero">#${pokemon.number}</span>
-            <span class="nome">${pokemon.name}</span>
-
-            <div class="detali">
-                <ol class="types">
-                ${pokemon.types.map((type) => 
-                    `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-=======
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 const maxRecords = 151
@@ -32,23 +15,10 @@ function convertPokemonToLi(pokemon) {
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
->>>>>>> 01cf3b3 (Atualização)
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
         </li>
-<<<<<<< HEAD
-    `;
-}
-
-
-const pokemonList = document.getElementById('pokemonList');
-pokeApi.getPokemons().then((pokemons = []) => { // Corrigido o nome da função
-    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join(''); // Uso correto de innerHTML
-});
-
-
-=======
     `
 }
 
@@ -75,7 +45,6 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
->>>>>>> 01cf3b3 (Atualização)
 
 //for (let i = 0; i < pokemons.length; i++) {//Geito mais dificil de fazer 
 //        const pokemon = pokemons[i];
